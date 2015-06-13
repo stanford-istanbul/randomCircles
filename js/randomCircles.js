21e37d9df677251a541e4188b9fb44e6f3266e60
Bryce Cronkite-Ratcliff <brycecr@gmail.com>
2015-06-13 12:05:04 -0700

    Splitting soln and problem part 1

--- js/randomCircles.js
+++ js/randomCircles.js
@@ -1,16 +1,8 @@
-function randomCircles_v0() {
-    const MIN_BALL_DIAMETER = 20;
-    var newElem = document.createElement('div');
-    newElem.className = 'ball';
-    var diameter = randomInt(MIN_BALL_DIAMETER, 150);
-    newElem.style.backgroundColor = randomColor();
-    newElem.style.width = newElem.style.height = diameter;
-    newElem.style.top = randomInt(0, tuval.offsetHeight-diameter);
-    newElem.style.left = randomInt(0, tuval.offsetWidth-diameter);
-    tuval.appendChild(newElem);
+function makeRandomCircles (canvas) {
+   canvas.onclick = randomCircles;    
 }
 
-function randomCircles_v1() {
+function randomCircles() {
     const MIN_BALL_DIAMETER = 20;
     var ball = new Oval();
     var diameter = randomInt(MIN_BALL_DIAMETER, 150);
